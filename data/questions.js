8cb96a45d39aff80211834081cd84202189eb443
fuy90y <vakitchiv@gmail.com>
2026-04-24 02:29:06 +0900

    はむにく

--- data/questions.js
+++ data/questions.js
@@ -76,7 +76,7 @@ export const questions = [
     prompt: 'お題: 「学校の七不思議」。書くならどっち？',
     choices: [
       { pole: 'R', text: '牛乳でお米を食えたこと' },
-      { pole: 'U', text: '肉みたいに泳いでた' },
+      { pole: 'U', text: '理系のハムスターが肉みたいに泳いでた' },
     ],
   },
   {
